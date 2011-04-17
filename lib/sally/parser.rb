388d103b7137d17fe0ab30a518890e74d5b3e9ff
Emerson Vinicius <talk@evinicius.com>
2011-04-17 19:27:00 -0300

    removed not used files

--- lib/sally/parser.rb
+++
@@ -1,9 +0,0 @@
-module Sally
-  class Parser
-    TARGET = /My target '(.*)'/
-
-    def self.target(string)
-      TARGET.match(string).captures[0]
-    end
-  end
-end
--- lib/sally/version.rb
+++
@@ -1,3 +0,0 @@
-module Sally
-  VERSION = "0.0.1"
-end
